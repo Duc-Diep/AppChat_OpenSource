cad7baa8c44f47ba2f30152a558a6f70677e7dfe
doanvu2000 <doanvu20122000@gmail.com>
2021-06-30 22:02:33 +0700

    update binding

--- app/src/main/java/group1/appchat_opensource/fragments/ForgotPassFragment.java
+++ app/src/main/java/group1/appchat_opensource/fragments/ForgotPassFragment.java
@@ -1,11 +1,22 @@
 package group1.appchat_opensource.fragments;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import org.jetbrains.annotations.NotNull;
+
+import group1.appchat_opensource.R;
+import group1.appchat_opensource.databinding.ForgotpassFragmentBinding;
+
 public class ForgotPassFragment extends Fragment {
+    ForgotpassFragmentBinding binding;
     public static ForgotPassFragment newInstance() {
 
         Bundle args = new Bundle();
@@ -15,8 +26,11 @@ public class ForgotPassFragment extends Fragment {
         return fragment;
     }
 
+    @Nullable
+    @org.jetbrains.annotations.Nullable
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate( inflater, R.layout.forgotpass_fragment,container,false );
+        return binding.getRoot();
     }
 }
