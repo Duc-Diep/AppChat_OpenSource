4b142cc7b6093ee49127fa0f4d8de6130fc80659
Đức Điệp <=>
2021-07-03 22:37:48 +0700

    create splash screen and folder animation

---
+++ app/src/main/java/group1/appchat_opensource/activity/SplashActivity.java
@@ -0,0 +1,38 @@
+package group1.appchat_opensource.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import group1.appchat_opensource.R;
+import group1.appchat_opensource.utils.AppUtils;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        checkNetwork();
+    }
+
+    private void checkNetwork() {
+        if (AppUtils.isAvailableNetword(this)){
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            },3000);
+        }else{
+            Toast.makeText(this, "No network", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
