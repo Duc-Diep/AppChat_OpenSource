07554e6b750bc3ae9d99c2c55e0afb5061f328f0
doanvu2000 <doanvu20122000@gmail.com>
2021-07-09 21:36:58 +0700

    section 09_07_2021

--- app/src/main/java/group1/appchat_opensource/fragments/LoginFragment.java
+++ app/src/main/java/group1/appchat_opensource/fragments/LoginFragment.java
@@ -1,6 +1,7 @@
 package group1.appchat_opensource.fragments;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,8 @@ import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import org.jetbrains.annotations.NotNull;
 
 import group1.appchat_opensource.R;
@@ -17,6 +20,7 @@ import group1.appchat_opensource.databinding.LoginFragmentBinding;
 
 public class LoginFragment extends Fragment {
     LoginFragmentBinding binding;
+    FirebaseAuth mAuth;
     public static LoginFragment newInstance() {
 
         Bundle args = new Bundle();
@@ -31,6 +35,19 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate( inflater, R.layout.login_fragment,container,false );
+        mAuth = FirebaseAuth.getInstance();
+
         return binding.getRoot();
+    }
+    public void signIn(){
+        String email = "";
+        String password = "";
+        if (!Patterns.EMAIL_ADDRESS.matcher( email ).matches()){
+            return;
+        }
+        if (password.isEmpty()){
+            return;
+        }
+
     }
 }
