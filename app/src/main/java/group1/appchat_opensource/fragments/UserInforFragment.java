6ae06a573db9e1e50875736a101e83ea4e9af1b7
Doan Duy Nam <68463333+Doannam2000@users.noreply.github.com>
2021-07-12 21:55:04 +0700

    Update Profile

--- app/src/main/java/group1/appchat_opensource/fragments/UserInforFragment.java
+++ app/src/main/java/group1/appchat_opensource/fragments/UserInforFragment.java
@@ -1,14 +1,12 @@
 package group1.appchat_opensource.fragments;
 
-import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
-import android.os.Build;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -97,7 +95,8 @@ public class UserInforFragment extends Fragment {
             manager.popBackStackImmediate();
         });
         binding.btnSignOut.setOnClickListener(v->signOut());
-        binding.imgChooseImg.setOnClickListener(v->{onClickRequestPermission();});
+
+        binding.imgChooseImg.setOnClickListener(v->{chooseImage();});
         binding.btnChangePassword.setOnClickListener(v -> {
             updatePassword();
         });
@@ -130,33 +129,6 @@ public class UserInforFragment extends Fragment {
         });
 
     }
-    private void onClickRequestPermission()
-    {
-        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
-        {
-            return;
-        }
-        if(getContext().checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
-        {
-            chooseImage();
-        }
-        else
-        {
-            String [] permission = {Manifest.permission.READ_EXTERNAL_STORAGE};
-            requestPermissions(permission,MY_REQUEST_CODE);
-
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-        {
-            chooseImage();
-        }
-    }
-
     private void changeName() {
         data  = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
         HashMap<String,Object> hm = new HashMap<>();
