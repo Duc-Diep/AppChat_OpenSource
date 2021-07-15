7e003cbda4343df69d01fadfde9f7cce99f228ae
doanvu2000 <doanvu20122000@gmail.com>
2021-07-15 23:09:31 +0700

    fix avatar sender

--- app/src/main/java/group1/appchat_opensource/fragments/ChatFragment.java
+++ app/src/main/java/group1/appchat_opensource/fragments/ChatFragment.java
@@ -1,6 +1,7 @@
 package group1.appchat_opensource.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
